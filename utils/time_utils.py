6728a7e93be4cf5e1b0a507cc9e117b5381f927b
mumeiaiueo <yoda3273@icloud.com>
2026-02-22 14:39:46 +0900

    Delete utils directory

--- utils/time_utils.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-def generate_slots(start, end, interval):
-    slots = []
-    now = start
-    while now < end:
-        slots.append(now.strftime("%H:%M"))
-        now += timedelta(minutes=interval)
-    return slots
