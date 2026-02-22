7caf2ffc9ffcb54985f5c0e2544736be71b2d625
mumeiaiueo <yoda3273@icloud.com>
2026-02-22 13:24:43 +0900

    Update create.py

--- commands/create.py
+++ commands/create.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from datetime import datetime
 from utils.time_utils import generate_slots
 from views.slot_view import SlotView
+from data import load_data, save_data
 
 def setup(bot):
 
@@ -17,15 +18,22 @@ def setup(bot):
                      end:str,
                      interval:app_commands.Choice[int]):
 
+        await interaction.response.defer()
+
         start_dt = datetime.strptime(start,"%H:%M")
         end_dt = datetime.strptime(end,"%H:%M")
 
         slots = generate_slots(start_dt,end_dt,interval.value)
 
-msg = "📅 予約枠\n"
-for s in slots:
-    msg += f"🟢 {s}\n"
+        msg = "📅 予約枠\n"
+        for s in slots:
+            msg += f"🟢 {s}\n"
 
-message = await interaction.followup.send(msg, view=SlotView(slots))
-data["message_id"] = message.id
-save_data(data)
+        message = await interaction.followup.send(
+            msg,
+            view=SlotView(slots)
+        )
+
+        data = load_data()
+        data["message_id"] = message.id
+        save_data(data)
