d93a9c3e876b00726332b430c3f8c9c2df0cbcef
mumeiaiueo <yoda3273@icloud.com>
2026-02-22 11:53:41 +0900

    Create reserve.py

---
+++ reserve.py
@@ -0,0 +1,11 @@
+import json
+
+DATA_FILE = "data.json"
+
+def load_data():
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
+
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f, indent=4)
