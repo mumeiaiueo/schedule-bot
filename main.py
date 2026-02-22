b19833b72406e90775cbcf0650802457072ba393
mumeiaiueo <yoda3273@icloud.com>
2026-02-22 17:19:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 class Bot(commands.Bot):
     async def setup_hook(self):
+        # コマンド登録
+        from commands.create import setup as create_setup
+        from commands.settings import setup as settings_setup  # notifysetがここなら
+        from commands.debug import setup as debug_setup
+
+        create_setup(self)
+        settings_setup(self)
+        debug_setup(self)
+
+        # リマインドループ開始
         from commands.remind import start_loop
         await start_loop(self)
 
-intents = discord.Intents.default()
-intents.message_content = True   # ←追加
+        await self.tree.sync()
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    await bot.tree.sync()
     print("起動完了:", bot.user)
-
-from commands.create import setup as create_setup
-from commands.settings import setup as settings_setup
-from commands.debug import setup as debug_setup
-
-create_setup(bot)
-settings_setup(bot)
-debug_setup(bot)
 
 bot.run(TOKEN)
