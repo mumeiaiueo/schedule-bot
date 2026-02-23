c55a5659698e755b4c920e9856946a92e19d4a26
mumeiaiueo <yoda3273@icloud.com>
2026-02-23 23:13:07 +0900

    db.py を更新

--- utils/db.py
+++ utils/db.py
@@ -3,7 +3,7 @@ import asyncpg
 async def init_db_pool(dsn: str):
     return await asyncpg.create_pool(
         dsn=dsn,
-        ssl="require",              # ← これが正解
+        ssl="require",
         statement_cache_size=0,
         min_size=1,
         max_size=5,
