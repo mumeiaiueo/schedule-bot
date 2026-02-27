698ff33e208bac335f0a55bf6914bc6814d2a771
mumeiaiueo <yoda3273@icloud.com>
2026-02-27 20:37:02 +0900

    setup_channel.py を更新

--- commands/setup_channel.py
+++ commands/setup_channel.py
@@ -4,5 +4,4 @@ import discord
 def register(tree: discord.app_commands.CommandTree, bot_client):
     @tree.command(name="setup_channel", description="このチャンネルで募集パネルを作成（ウィザード）")
     async def setup_channel(interaction: discord.Interaction):
-        # すぐ返す（反応しません対策）
         await bot_client.start_setup_wizard(interaction)
